c657bbe1fa1cbb44510343010c2ae691f79ab788
smr-j <samar.j@northeastern.edu>
2024-08-14 13:39:15 -0400

    updated terraform files to remove unnecessary resources

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ A repository for the CS6620 Cloud Computing Final Project
 ## Sources
 https://barometricpressure.app/content/understanding-high-low-pressure-readings
 https://forecast.weather.gov/glossary.php?word=PRES
+https://stackoverflow.com/questions/70407525/terraform-gives-errors-failed-to-load-plugin-schemas
+https://docs.localstack.cloud/tutorials/s3-static-website-terraform/
+https://docs.localstack.cloud/user-guide/integrations/terraform/
--- src/main.tf
+++ src/main.tf
@@ -4,34 +4,12 @@ resource "aws_s3_bucket" "s3_bucket" {
   tags   = var.tags
 }
 
-resource "aws_s3_bucket_website_configuration" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  index_document {
-    suffix = "templates/map.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-}
 
 resource "aws_s3_bucket_acl" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
   acl    = "public-read"
 }
 
-resource "aws_s3_object" "object_www" {
-  depends_on   = [aws_s3_bucket.s3_bucket]
-  for_each     = fileset("${path.root}", "www/*.html")
-  bucket       = var.bucket_name
-  key          = basename(each.value)
-  source       = each.value
-  etag         = filemd5("${each.value}")
-  content_type = "text/html"
-  acl          = "public-read"
-}
 
 # resource "aws_s3_object" "object_assets" {
 #   depends_on = [aws_s3_bucket.s3_bucket]
